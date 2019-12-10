ac32cb302cfa36957d30c32c4b6452c629447b0e
Carlos Salazar <cs94490@gmail.com>
2019-12-09 23:40:48 -0500

    Mi primer Cambio con Git

---
+++ src/main/java/co/edu/usbcali/smc/App.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.smc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bienvenidos Compañeros USB!" );
+    }
+}
